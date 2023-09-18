ee0c900e6c332920d658fc4e66cb592bdd970c3a
Shivansh Aggarwal <saggarwal24@wisc.edu>
2023-09-18 15:51:03 -0500

    Wrote code for sub problem

--- flow_robust_multivariate_tree.py
+++ flow_robust_multivariate_tree.py
@@ -8,13 +8,8 @@ import gurobipy as gp
 from gurobipy import GRB
 from .decision_tree import MultivariateDecisionTree
 
-class RBT(ClassifierMixin, BaseEstimator):
-    """Our implementation of FlowOCT (Aghaei et al. (2021)).
-    
-    This implementation follows the formulation (7). We do not include
-    the penalty term in the objective (no lambda), instead we use the
-    sparsity constraint to restrict the number of branching nodes.
-    
+class RDT(ClassifierMixin, BaseEstimator):
+    """
     Parameters
     ----------
     time_limit : positive float, default=None
@@ -52,7 +47,7 @@ class RBT(ClassifierMixin, BaseEstimator):
         
         Returns
         -------
-        self : RBT
+        self : RDT
             Fitted estimator.
         """
 
@@ -66,7 +61,7 @@ class RBT(ClassifierMixin, BaseEstimator):
         self._set_gurobi_params()
         
         # Solve MIP model
-        callback = RBT._callback
+        callback = RDT._callback
         self.model_.optimize(callback)
         
         # Construct the decision tree
@@ -148,6 +143,8 @@ class RBT(ClassifierMixin, BaseEstimator):
 
         t = model.addVars(n_samples, ub=1)
 
+        model._vars = c, a, a_cap, b, t
+
         obj_fn = t.sum()
         model.setObjective(obj_fn, GRB.MAXIMIZE)
 
@@ -176,3 +173,101 @@ class RBT(ClassifierMixin, BaseEstimator):
             for n in branch_nodes
             for i in  n_features
         ))
+
+    def _set_gurobi_params(self):
+        """Set Gurobi parameters."""
+        model = self.model_
+        model.Params.LogToConsole = self.verbose
+        model.Params.LazyConstraints = True
+        if self.time_limit is not None:
+            model.Params.TimeLimit = self.time_limit
+        model.Params.MIPGap = 0.0
+        # Need to reduce desired gap (exactly 1) by a small amount due to numerical issues
+        model.Params.MIPGapAbs = 0.999
+
+    @staticmethod
+    def _callback(model, where):
+        """Gurobi callback. Adds lazy cuts."""
+        if where == GRB.Callback.MIPSOL:
+            c, a, a_cap, b, t = model._vars
+            a_vals = model.cbGetSolution(a)
+            b_vals = model.cbGetSolution(b)
+            c_vals = model.cbGetSolution(c)
+            X, y = model._X_y
+            branch_nodes, leaf_nodes, arcs = model._flow_graph
+            n_samples, n_features = X.shape
+            classes = unique_labels(y)
+
+            lam = np.array([0.1 for i in n_features])
+            budget = 10
+
+            mu = np.array([[i, 0] for i in n_samples])
+            perturb = np.array([[0 for j in n_features] for i in n_samples])
+
+            for i, x in enumerate(X):
+                j = 1
+                while j <= len(branch_nodes):
+                    if np.dot(a_vals[j], x) > b_vals[j]:
+                        j = 2*j + 1
+                    else:
+                        j = 2*j
+                if c_vals[j, y[i]] != 1:
+                    continue
+
+                min_mu = 1000000
+                for k in leaf_nodes:
+                    if np.argmax(c_vals[k]) == y[i]:
+                        continue
+
+                    sub_model = gp.Model()
+                    perturb_var = sub_model.addVars(n_features)
+                    perturb_cap_var = sub_model.addVars(n_features)
+
+                    obj_fn = np.dot(lam, perturb_cap_var)
+                    sub_model.setObjective(obj_fn, GRB.MINIMIZE)
+
+                    sub_model.addConstrs((
+                        perturb_cap_var[n] >= perturb_var[n]
+                        for n in n_features
+                    ))
+
+                    sub_model.addConstrs((
+                        perturb_cap_var[n] >= perturb_var[n]
+                        for n in n_features
+                    ))
+
+                    parent = k // 2
+                    child = k % 2
+                    while parent >= 1:
+                        if child == 0:
+                            sub_model.addConstrs((
+                                np.dot(a_vals[parent], perturb_var) < b_vals[parent] - np.dot(a_vals[parent], x)
+                            ))
+                        else:
+                            sub_model.addConstrs((
+                                np.dot(a_vals[parent], perturb_var) >= b_vals[parent] - np.dot(a_vals[parent], x)
+                            ))
+                        
+                        parent = k // 2
+
+                    sub_model.optimize()
+
+                    opt_sol = sub_model.getVars()
+                    if sub_model.objVal <= min_mu:
+                        min_mu = sub_model.objVal
+                        mu[i, 1] = min_mu
+                        for v in opt_sol:
+                            if v.varName == "perturb_var":
+                                perturb[i] = v.x
+            
+            mu = mu[mu[:, 1].argsort()]
+            perturb_set = np.array([])
+            total_cost = 0
+            for i in n_samples:
+                if total_cost + mu[i, 1] <= budget:
+                    total_cost += mu[i, 1]
+                    perturb_set.append(mu[i, 0])
+                else:
+                    break
+            
+            
