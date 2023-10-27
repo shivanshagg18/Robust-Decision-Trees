902c8a43cd1306dc0f67da8d271d337d1e808728
Shivansh Aggarwal <saggarwal24@wisc.edu>
2023-10-26 19:07:00 -0500

    Added code for lazy cuts

--- flow_robust_multivariate_tree.py
+++ flow_robust_multivariate_tree.py
@@ -6,7 +6,8 @@ from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
 from sklearn.utils.multiclass import unique_labels
 import gurobipy as gp
 from gurobipy import GRB
-from .decision_tree import MultivariateDecisionTree
+from decision_tree import MultivariateDecisionTree
+import random
 
 # TODO (not urgent) even though index variables can be named anything, I strongly recommend sticking to the following conventions:
 # - Decision tree nodes: t (note that the decision variables are also named t)
@@ -37,7 +38,8 @@ class RDT(ClassifierMixin, BaseEstimator):
         Time (in seconds) taken to fit the model.
     """
 
-    def __init__(self, time_limit=None, verbose=False, epsilon=0.005):
+    def __init__(self, time_limit=None, verbose=0, epsilon=0.005, max_depth=5):
+        self.max_depth = max_depth
         self.time_limit = time_limit
         self.verbose = verbose
         self.epsilon = epsilon
@@ -69,11 +71,11 @@ class RDT(ClassifierMixin, BaseEstimator):
         self._set_gurobi_params()
         
         # Solve MIP model
-        callback = RDT._callback
-        self.model_.optimize(callback)
+        self.model_.optimize()
+        self.cuts()
         
         # Construct the decision tree
-        self.decision_tree_ = self._construct_decision_tree()
+        # self.decision_tree_ = self._construct_decision_tree()
         
         self.fit_time_ = time.perf_counter() - start_time
         
@@ -124,6 +126,7 @@ class RDT(ClassifierMixin, BaseEstimator):
         
         branch_nodes = list(range(1, 2**self.max_depth))
         leaf_nodes = list(range(2**self.max_depth, 2**(self.max_depth+1)))
+        nodes = list(range(1, 2**(self.max_depth+1)))
         arcs = (
             [('s', 1)]
             + [(n, 2*n) for n in branch_nodes]
@@ -149,24 +152,39 @@ class RDT(ClassifierMixin, BaseEstimator):
         
         model = gp.Model()
         model._X_y = X, y
-        model._flow_graph = branch_nodes, leaf_nodes, arcs
+        model._flow_graph = branch_nodes, leaf_nodes, nodes, arcs
         model._ancestors = left_ancestors, right_ancestors
 
-        c = model.addVars(leaf_nodes, classes, vtype=GRB.BINARY)
-        a = model.addVars(branch_nodes, n_features, lb=-1, ub=1)
-        a_cap = model.addVars(branch_nodes, n_features, lb=0, ub=1)
-        b = model.addVars(branch_nodes, lb=-1, ub=1)
+        c = model.addVars(leaf_nodes, classes, vtype=GRB.BINARY, name="c")
+        a = model.addVars(branch_nodes, n_features, lb=-1, ub=1, name="a")
+        a_cap = model.addVars(branch_nodes, n_features, lb=0, ub=1, name="a_cap")
+        b = model.addVars(branch_nodes, lb=-1, ub=1, name="b")
 
-        t = model.addVars(n_samples, vtype=GRB.BINARY, ub=1)
+        g = model.addVars(n_samples, vtype=GRB.BINARY, ub=1, name="t")
 
-        model._vars = c, a, a_cap, b, t
+        model._vars = c, a, a_cap, b, g
 
-        lam = np.array([0.1 for i in n_features])
-        budget = 10
+        for t in leaf_nodes:
+            for label in classes:
+                if t % len(classes) == label:
+                    c[t, label].Start = 1
+                else:
+                    c[t, label].Start = 0
+
+        for n in branch_nodes:
+            for i in range(n_features):
+                a_cap[n, i].Start = 1/n_features
+                a[n, i].Start = 1/n_features
+
+        for n in branch_nodes:
+            b[n].Start = random.uniform(0, 1)
+
+        lam = np.array([0.1 for i in range(n_features)])
+        budget = 5
         model._cost = lam, budget
         model._epsilon = self.epsilon
 
-        obj_fn = t.sum()
+        obj_fn = g.sum()
         model.setObjective(obj_fn, GRB.MAXIMIZE)
 
         model.addConstrs((
@@ -191,50 +209,55 @@ class RDT(ClassifierMixin, BaseEstimator):
             for i in range(n_features)
         ))
 
+        return model
+
     def _set_gurobi_params(self):
         """Set Gurobi parameters."""
         model = self.model_
         model.Params.LogToConsole = self.verbose
-        model.Params.LazyConstraints = True
+        model.Params.OutputFlag = self.verbose
         if self.time_limit is not None:
             model.Params.TimeLimit = self.time_limit
         model.Params.MIPGap = 0.0
         # Need to reduce desired gap (exactly 1) by a small amount due to numerical issues
         model.Params.MIPGapAbs = 0.999
 
-    @staticmethod
-    def _callback(model, where):
+    
+    def cuts(self):
         """Gurobi callback. Adds lazy cuts."""
-        if where == GRB.Callback.MIPSOL:
-            c, a, a_cap, b, t = model._vars
-            a_vals = model.cbGetSolution(a)
-            b_vals = model.cbGetSolution(b)
-            c_vals = model.cbGetSolution(c)
-            X, y = model._X_y
-            branch_nodes, leaf_nodes, arcs = model._flow_graph
-            left_ancestors, right_ancestors = model._ancestors
-            n_samples, n_features = X.shape
-            classes = unique_labels(y)
-            lam, budget = model._cost
-            epsilon = model._epsilon
+        model = self.model_
 
+        c, a, a_cap, b, g = model._vars
+
+        X, y = model._X_y
+        branch_nodes, leaf_nodes, nodes, arcs = model._flow_graph
+        left_ancestors, right_ancestors = model._ancestors
+        n_samples, n_features = X.shape
+        classes = unique_labels(y)
+        lam, budget = model._cost
+        epsilon = model._epsilon
+
+        stop = True
+
+        while(stop == True):
+            a_vals = model.getAttr('X', a)
+            b_vals = model.getAttr('X', b)
+            c_vals = model.getAttr('X', c)
+            
             mu_i_xi = []  # List of tuples (mu, i, xi), one for every sample
-
             for i, x in enumerate(X):
-                j = 1
-                while j <= len(branch_nodes):
-                    # I don't think a_vals[j] will work the way you want, try replacing with something like this:
-                    # [a_vals[t,j] for j in range(n_features)]  # t = branch node, j indexes features
-                    if np.dot(a_vals[j], x) > b_vals[j]:
-                        j = 2*j + 1
+                t = 1
+                while t <= len(branch_nodes):
+                    if np.dot([a_vals[t, f] for f in range(n_features)], x) > b_vals[t]:
+                        t = 2*t + 1
                     else:
-                        j = 2*j
-                # This is confusing to me, why do we continue if c == 0, shouldn't it be c == 1 (sample is correctly classified)?
-                if c_vals[j, y[i]] < 0.5:
+                        t = 2*t
+                        
+                if c_vals[t, y[i]] < 0.5:
                     continue
-
+                    
                 best_mu = float('inf')
-                best_xi = [0 for j in n_features]
+                best_xi = [0 for f in range(n_features)]
                 for t in leaf_nodes:
                     if c_vals[t, y[i]] > 0.5:
                         continue
@@ -243,9 +266,7 @@ class RDT(ClassifierMixin, BaseEstimator):
                     perturb_var = sub_model.addVars(n_features, lb=-GRB.INFINITY, ub=GRB.INFINITY)
                     perturb_cap_var = sub_model.addVars(n_features, lb=0, ub=GRB.INFINITY)
 
-                    # If obj_fn doesn't work (I suspect it won't) try this:
-                    # obj_fn = gp.quicksum(lam[j]*perturb_cap_var[j] for j in range(n_features))
-                    obj_fn = np.dot(lam, perturb_cap_var)
+                    obj_fn = gp.quicksum(lam[f]*perturb_cap_var[f] for f in range(n_features))
                     sub_model.setObjective(obj_fn, GRB.MINIMIZE)
 
                     sub_model.addConstrs((
@@ -257,30 +278,73 @@ class RDT(ClassifierMixin, BaseEstimator):
                         perturb_cap_var[n] >= -perturb_var[n]
                         for n in range(n_features)
                     ))
-                    
+
                     sub_model.addConstrs((
-                        np.dot(a_vals[ancestor], perturb_var) <= b_vals[ancestor] - np.dot(a_vals[ancestor], x)
+                        gp.quicksum(a_vals[ancestor, f]*perturb_var[f] for f in range(n_features)) <= b_vals[ancestor] - gp.quicksum(a_vals[ancestor, f]*x[f] for f in range(n_features))
                         for ancestor in left_ancestors[t]
                     ))
                     sub_model.addConstrs((
-                        np.dot(a_vals[ancestor], perturb_var) >= b_vals[ancestor] + epsilon - np.dot(a_vals[ancestor], x)
+                        gp.quicksum(a_vals[ancestor, f]*perturb_var[f] for f in range(n_features)) >= b_vals[ancestor] + epsilon - gp.quicksum(a_vals[ancestor, f]*x[f] for f in range(n_features))
                         for ancestor in right_ancestors[t]
                     ))
+                    
+                    sub_model.Params.LogToConsole = False
 
                     sub_model.optimize()
 
-                    if sub_model.objVal < best_mu:
+                    
+                    if sub_model.status == 2 and sub_model.objVal < best_mu:
                         best_mu = sub_model.objVal
                         best_xi = sub_model.getAttr('X', perturb_var)
-                
+
                 mu_i_xi.append((best_mu, i, best_xi))
-            
+
             perturb_set = []
             total_cost = 0
             for mu, i, xi in sorted(mu_i_xi):
                 if total_cost + mu <= budget:
                     total_cost += mu
-                    perturb_set.append((i, xi))
+                    perturb_set.append([i, xi])
                 else:
                     break
+            
+            benders_cut_rhs = gp.LinExpr()
+            for x_perturb in perturb_set:
+                x = np.copy(X[x_perturb[0]])
+                for j in x_perturb[1].keys():
+                    x[j] += x_perturb[1][j]
+                t = 1
+                while t <= len(branch_nodes):
+                    gamma = model.addVars([1], vtype=GRB.BINARY)
+                    if np.dot([a_vals[t, f] for f in range(n_features)], x) > b_vals[t]:
+                        model.addConstr((gamma[1] == 1) >> (np.dot([a[t, f] for f in range(n_features)], x) <= b[t]))
+                        model.addConstr((gamma[1] == 0) >> (np.dot([a[t, f] for f in range(n_features)], x) >= b[t] + epsilon))
+                        benders_cut_rhs.add(gamma[1])
+                        t = 2*t + 1
+                    else:
+                        model.addConstr((gamma[1] == 1) >> (np.dot([a[t, f] for f in range(n_features)], x) >= b[t] + epsilon))
+                        model.addConstr((gamma[1] == 0) >> (np.dot([a[t, f] for f in range(n_features)], x) <= b[t]))
+                        benders_cut_rhs.add(gamma[1])
+                        t = 2*t
+                benders_cut_rhs.add(c[t, y[x_perturb[0]]])
+
+            model.addConstr(g.sum() <= benders_cut_rhs)
+            model.optimize()
+
+            acc_count = 0
+            for i, x in enumerate(X):
+                t = 1
+                while t <= len(branch_nodes):
+                    if np.dot([a_vals[t, f] for f in range(n_features)], x) > b_vals[t]:
+                        t = 2*t + 1
+                    else:
+                        t = 2*t
+                        
+                if c_vals[t, y[i]] > 0.5:
+                    acc_count += 1
+
+            stop = (model.objVal >= acc_count)
+
+            print("Accuracy: ", acc_count/len(X))
+            print("Objective Value: ", model.objVal)
             
